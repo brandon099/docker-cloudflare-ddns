ce15af87aa7e7d85516b2ec23ffaa0f7ffe04760
Brandon Clifford <brandon.clifford@vivint.com>
2019-07-28 20:07:39 -0600

    Remove proxied true setting in the update command

--- run.sh
+++ run.sh
@@ -32,7 +32,7 @@ if [ "$ip" == "$cf_ip" ]; then
   exit 0
 fi
 
-update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_id" -H "Authorization: Bearer $api_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_id\",\"type\":\"A\",\"proxied\":true,\"name\":\"$record_name\",\"content\":\"$ip\"}")
+update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$record_id" -H "Authorization: Bearer $api_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_id\",\"type\":\"A\",\"proxied\":false,\"name\":\"$record_name\",\"content\":\"$ip\"}")
 
 case "$update" in
 *"\"success\":false"*)
